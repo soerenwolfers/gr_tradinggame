f4c26d6682bd79ab31e2d79df9f9214f551f8422
Soeren Wolfers <soeren.wolfers@gmail.com>
2025-05-22 01:37:20 +0100

    asdf

--- src/gr_tradinggame/coding/example_gui.py
+++ src/gr_tradinggame/coding/example_gui.py
@@ -1,3 +1,3 @@
 from gr_tradinggame.coding.gui import CodingGame
-game = CodingGame(max_rounds=2000, cooldown=3, duration=10)
+game = CodingGame(rounds=2000, cooldown=3, length_in_seconds=10)
 game.play()
